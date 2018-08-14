a722b6a2136a35391c948866f6cc4b577d306ba5
Henrique Mateus Dalfovo Campos <hmdcampos@gmail.com>
2018-08-14 15:12:52 -0300

    trabalhando no banco de dados

--- TrabalhoFinal-Restaurante/TrabalhoFinal-Restaurante/DataBase/Funcionarios.sql
+++ TrabalhoFinal-Restaurante/TrabalhoFinal-Restaurante/DataBase/Funcionarios.sql
@@ -1,6 +1,16 @@
 ﻿CREATE TABLE funcionarios(
-	id
-	nome
+	id					INT IDENTITY(01,1),
+	nome				VARCHAR
 	sobrenome
+	senha
+	celular
+	data_nascimento
+	cpf
+	estado
+	cidade
+	bairro
+	logradouro
+	cep
+	cargo
 
 );
